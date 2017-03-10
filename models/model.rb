4f1f720b80e4272756dcf3397892a6bc874b4033
jozefh9045 <jozefh9045@hstat.org>
2017-03-10 19:00:26 +0000

    start making movie class

--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,20 @@
 require 'omdbapi'
 require 'pp'
 
+class Movie
+    attr_reader :title, :year, :rating, :rated, :runtime, :genre, :releasedate, :seasons
+    
+    def initialize(title, year, rating, rated, runtime, genre, releasedate, seasons)
+        @title = title
+        @year = year
+        @rating = rating
+        @rated = rated
+        @runtime = runtime
+        @genre = genre
+        @releasedate = releasedate
+        @season = season
+    end
+end
+
 test_movie = OMDB.title("Game of Thrones")
-pp test_movie.year
+pp test_movie
