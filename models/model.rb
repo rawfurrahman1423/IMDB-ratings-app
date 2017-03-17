fc68b089437a382fdf6f83c43948b7fe34c3a0e6
rawfurrahman1423 <rawfurr1423@hstat.org>
2017-03-17 04:10:28 +0000

    comments+

--- models/model.rb
+++ models/model.rb
@@ -1,12 +1,16 @@
-require 'omdbapi'
+#These gems have to be activated so that the program can run
+require 'omdbapi' 
 require 'pp'
 
 class Movie
-    attr_reader :title, :year, :rating, :rated, :runtime, :genre, :releasedate, :seasons #different holder of information
+    attr_reader :title, :year, :rating, :rated, :runtime, :genre, :releasedate, :seasons #different temporary holders of information
     
     #all variables listed below that gets information based on what the user types into movie name
     def initialize(title) 
         @title = title
+        #test_movie = OMDB.title("Logan")
+        #pp test_movie
+        #above is the format that the title is taken from the user and information placed into variables
         movie = OMDB.title(title)
         @year = movie.year
         @rating = movie.imdb_rating
@@ -18,5 +22,4 @@ class Movie
     end
 end
 
-#test_movie = OMDB.title("Logan")
-#pp test_movie
+
